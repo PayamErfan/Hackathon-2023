94cdb8876d022655d653ed306e7f9ee308ae6a05
PayamErfan <payamerfan@berkeley.edu>
2023-06-18 10:29:33 -0700

    woowie

--- Autobot.py
+++ Autobot.py
@@ -1,10 +1,33 @@
 import streamlit as st
+import requests
 def process_user_message(message):
     # You can implement your chatbot logic here to generate a response
     # Example: Send an API request to a backend server and handle the response
-    # Once you have the response, return it as the bot's message
+    # Once you have the response, return it as the bot's message\
+    send_api_request()
     bot_response = "This is a bot response"
     return bot_response
+
+def send_api_request():
+    url = 'https://example.com/api'  # Replace with your API endpoint URL
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Request was successful
+            data = response.json()
+            # Handle the response data
+            print("Response:", data)
+        else:
+            # Request failed
+            print("Request failed with status code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        # An error occurred during the request
+        print("Request failed:", e)
+
+# Call the function to send the API request
+send_api_request()
+
 
 def main():
     st.markdown("<h1 style='text-align: center;'>LLMD</h1>", unsafe_allow_html=True)
